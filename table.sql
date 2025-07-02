f9a1c11cf150b6ace0e5c198fdc6da3d31fc6618
Sin <yosshi861@gmail.com>
2025-07-02 20:49:27 +0900

    userテーブル定義文の追加

--- table.sql
+++ table.sql
@@ -1,11 +1,26 @@
 -- ＜テーブル作成用sql＞
 
 -- データベースの作成と選択
+create database if not exists DB2025; --名前は仮置きです
+use DB2025;
 
 -- テーブルが既に存在しているなら削除する
+drop table if exists user;
 
 -- ユーザーテーブル作成
-
+create table user(
+    userID int not null AUTO_INCREMENT,
+    personal_number varchar(20) not null,
+    l_name varchar(20) not null,
+    f_name varchar(20) not null,
+    affiliation varchar(20) not null,
+    tell varchar(20),
+    mail varchar(20),
+    addr varchar(50),
+    lastupdate date default now(),
+    deleteflag boolean default False,
+    primary key(userID)
+);
 -- 体調記録テーブル作成
 
 -- 行動記録テーブル作成
@@ -13,3 +28,4 @@
 -- コロナテーブル作成
 
 -- 作成したテーブルの確認
+desc user;
