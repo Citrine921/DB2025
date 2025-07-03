321ab5ddf7ef52a15978a1bec99c15f781685679
keishou-cloud <kei.shou.one.one.6@gmail.com>
2025-07-03 14:21:06 +0900

    コロナテーブル作成

--- table.sql
+++ table.sql
@@ -12,4 +12,26 @@
 
 -- コロナテーブル作成
 
+--DROP TABLE if EXISTS Covid;
+CREATE TABLE Covid (
+    covid_ID INT NOT NULL AUTO_INCREMENT,
+    personal_number INT NOT NULL, 
+    condition INT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NOT NULL,
+    medical_insttution VARCHAR(30),
+    doctor_name VARCHAR(20),
+    lastupdate DATETIME,
+    delflag BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (covid_ID),
+    FOREIGN KEY (personal_number) 
+		REFERENCES user(personal_number)
+		ON DELETE cascade
+		ON UPDATE cascade
+);
+
+--DESC Covid ;
+--SELECT *
+--FROM Covid;
+
 -- 作成したテーブルの確認
