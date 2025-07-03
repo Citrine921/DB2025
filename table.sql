14efbd2d67591c19d95749cd947066a47ff0c126
Citrine <m.kotaro921@gmail.com>
2025-07-03 14:25:00 +0900

    first_comit

--- table.sql
+++ table.sql
@@ -7,7 +7,23 @@
 -- ユーザーテーブル作成
 
 -- 体調記録テーブル作成
-
+CREATE TABLE DB2025.health_records (
+    health_record_ID INT AUTO_INCREMENT PRIMARY KEY,
+    personal_number TEXT NOT NULL,
+    date DATE NOT NULL,
+    datetime BOOLEAN NOT NULL,
+    temperature FLOAT NOT NULL,
+    joint_muscle_pain BOOLEAN NOT NULL DEFAULT 0,
+    fatigue BOOLEAN NOT NULL DEFAULT 0,
+    headache BOOLEAN NOT NULL DEFAULT 0,
+    sore_throat BOOLEAN NOT NULL DEFAULT 0,
+    shortness_of_breath BOOLEAN NOT NULL DEFAULT 0,
+    cough_sneeze BOOLEAN NOT NULL DEFAULT 0,
+    nausea_vomiting BOOLEAN NOT NULL DEFAULT 0,
+    abdominal_pain_diarrhea BOOLEAN NOT NULL DEFAULT 0,
+    taste_disorder BOOLEAN NOT NULL DEFAULT 0,
+    smell_disorder BOOLEAN NOT NULL DEFAULT 0
+);
 -- 行動記録テーブル作成
 
 -- コロナテーブル作成
